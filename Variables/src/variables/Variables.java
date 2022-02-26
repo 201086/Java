7117506a3cb91a60d9982077e367517b273742f6
Jesus Roberto <34330376+201086@users.noreply.github.com>
2022-02-26 12:36:07 -0700

    Add files via upload
    Ejemplos de variables en Java

---
+++ Variables/src/variables/Variables.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package variables;
+
+/**
+ *
+ * @author Maribel
+ */
+public class Variables {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int version = 9;
+        int versionJava = 8;
+        float versionEspecifica = 9.8f;
+        
+        char letra = 'a';
+        char numero = 97;
+        
+        System.out.println(version);
+        System.out.println(versionJava);
+        
+        System.out.println(letra);
+        System.out.println(numero);
+        
+        System.out.println(versionEspecifica);
+    }
+    
+}

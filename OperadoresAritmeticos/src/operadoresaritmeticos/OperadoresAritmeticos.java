e550c3cdc5881c635c79875806bd6a15e07bd3d9
Jesus Roberto <34330376+201086@users.noreply.github.com>
2022-02-26 13:15:59 -0700

    Add files via upload
    Operadores Aritmeticos

---
+++ OperadoresAritmeticos/src/operadoresaritmeticos/OperadoresAritmeticos.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package operadoresaritmeticos;
+
+/**
+ *
+ * @author Maribel
+ */
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int variableX = 50;
+        int variableY = 10;
+        int resultado = 0;
+        
+        resultado = variableX + variableY;
+        System.out.println(resultado);
+        
+        resultado = variableX - variableY;
+        System.out.println(resultado);
+        
+        resultado = variableX * variableY;
+        System.out.println(resultado);
+        
+        resultado = variableX / variableY;
+        System.out.println(resultado);
+    }
+    
+}
